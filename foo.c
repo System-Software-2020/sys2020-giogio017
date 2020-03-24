35dd42d348c9f9d7c40a7c10fe7491b8534afd99
Giovanni-P-Meloni <gio.mel@hotmail.com>
2020-03-24 15:07:58 -0300

    ex2done

---
+++ foo.c
@@ -0,0 +1,4 @@
+//NAo consegui entender o codigo gerado em assembly, tentei usar os comandos ''-M intel -d'', mas nada aconteceu
+int foo(int a, int b){
+    return a+b;
+}
---
+++ main001.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "foo.h"
+
+#define MAX 10
+#define VAL  5
+
+//extern int foo(int, int);
+
+int main ()
+{
+  int i;
+
+  for (i=0; i < MAX; i++)
+    printf ("foo(%d,%d) = %2d\n", i, VAL, foo(i, VAL));
+  
+  return EXIT_SUCCESS;
+}
